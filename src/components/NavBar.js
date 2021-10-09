cffc77d92118a2c8c5ff2cddb513af532103af34
sumere4ny <sumere4ny@gmail.com>
2021-10-09 12:29:02 +0300

    Some hotfixes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import AppContext from '../contexts/context';
 
 const NavBar = ({ onLogout }) => {
   const { isAuth } = useContext(AppContext);
-  console.log(isAuth);
+
   return (
   <nav className="navigation__bar">
   <Link className="navigation__link" to="/">Задачи</Link>
